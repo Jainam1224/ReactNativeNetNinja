8bfc2a79b6fe0de20531bfdd926d0454544dc9c2
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-05-25 03:14:27 +0530

    lesson 14 - making an auth context

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,13 +2,14 @@ import { Stack } from "expo-router";
 import { Colors } from "../constants/Colors";
 import { useColorScheme } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { UserProvider } from "../contexts/userContext";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const theme = Colors[colorScheme] ?? Colors.light;
 
   return (
-    <>
+    <UserProvider>
       <StatusBar value="auto" />
       <Stack
         // applied to each individial screen, i.e. common options for each screen
@@ -22,7 +23,7 @@ export default function RootLayout() {
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
       </Stack>
-    </>
+    </UserProvider>
   );
 }
 
