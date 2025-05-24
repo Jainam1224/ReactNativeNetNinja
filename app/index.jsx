4798b95f0384b7df80cc9a8cfa6ba1496086c142
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-05-25 00:22:04 +0530

    lesson 6 - themed based ui common themed components

--- app/index.jsx
+++ app/index.jsx
@@ -1,26 +1,34 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, Image } from "react-native";
 import React from "react";
-import Logo from "../assets/img/logo_light.png";
 import { Link } from "expo-router";
+
+import ThemedView from "../components/ThemedView";
+import ThemedLogo from "../components/ThemedLogo";
+import Spacer from "../components/Spacer";
+import ThemedText from "../components/ThemedText";
 
 const Home = () => {
   return (
-    // We are using View, Text which are the native component
-    // and not using HTML tags like div, paragraph etc.
-    <View style={styles.container}>
-      <Image source={Logo} style={styles.img} />
+    <ThemedView style={styles.container}>
+      <ThemedLogo />
+      <Spacer />
 
-      <Text style={styles.title}>The Number 1</Text>
+      <ThemedText style={styles.title} title={true}>
+        The Number 1
+      </ThemedText>
 
-      <Text style={{ marginTop: 10, marginBottom: 30 }}>Reading List App</Text>
+      <ThemedText style={{ marginTop: 10, marginBottom: 30 }}>
+        Reading List App
+      </ThemedText>
 
       <Link href="/about" style={styles.link}>
-        About Page
+        <ThemedText>About Page</ThemedText>
       </Link>
+
       <Link href="/contact" style={styles.link}>
-        Contact Page
+        <ThemedText>Contact Page</ThemedText>
       </Link>
-    </View>
+    </ThemedView>
   );
 };
 
@@ -28,7 +36,6 @@ export default Home;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#e0dfe8",
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
