c124e7de33f341a70afd0e9d8aa8f4758e9f7593
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-05-24 22:54:17 +0530

    lession-2

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,50 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
+import Logo from "../assets/img/logo_light.png";
 
 const Home = () => {
   return (
     // We are using View, Text which are the native component
     // and not using HTML tags like div, paragraph etc.
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.img} />
+
+      <Text style={styles.title}>The Number 1</Text>
+
+      <Text style={{ marginTop: 10, marginBottom: 30 }}>Reading List App</Text>
+
+      <View style={styles.card}>
+        <Text>Hello, this is a Card</Text>
+      </View>
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#e0dfe8",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  img: {
+    marginVertical: 20,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  card: {
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 5,
+    boxShadow: "4px 4px rgba(0,0,0,0.1)",
+  },
+});
+
+// We have components dividesd as:
+// 1. Full page component - home page, product page etc.
+// 2. Custom build component - custom button, text etc.
+// 3. Built-in components - View, Text etc.
