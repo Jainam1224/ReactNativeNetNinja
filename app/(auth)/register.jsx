2daa38b7b2b856ab0727ab702abf8d74e632b399
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-05-25 00:59:17 +0530

    lesson 8 - pressable button

--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -4,14 +4,30 @@ import { Link } from "expo-router";
 import ThemedView from "../../components/ThemedView";
 import ThemedText from "../../components/ThemedText";
 import Spacer from "../../components/Spacer";
+import ThemedButton from "../../components/ThemedButton";
 
 const Register = () => {
+  const handleSubmit = async () => {
+    console.log("register form submitted");
+  };
+
   return (
     <ThemedView style={styles.container}>
       <Spacer />
       <ThemedText title={true} style={styles.title}>
         Register an Account
       </ThemedText>
+
+      {/* <Pressable
+        onPress={handleSubmit}
+        style={({ pressed }) => [styles.btn, pressed && styles.pressed]}
+      >
+        <Text style={{color: "#f2f2f2"}}>Register</Text>
+      </Pressable> */}
+
+      <ThemedButton onPress={handleSubmit}>
+        <ThemedText style={{ color: "#f2f2f2" }}>Register</ThemedText>
+      </ThemedButton>
 
       <Spacer height={100} />
       <Link href="/login" replace>
@@ -27,6 +43,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    alignItems: "center",
   },
   title: {
     textAlign: "center",
