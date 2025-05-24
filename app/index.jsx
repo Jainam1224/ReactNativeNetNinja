2a971d7f07d805a9bf6495c9650e58e2ff688ccf
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-05-24 21:50:56 +0530

    lesson-1

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Home = () => {
+  return (
+    // We are using View, Text which are the native component
+    // and not using HTML tags like div, paragraph etc.
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
