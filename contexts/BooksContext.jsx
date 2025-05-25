ff42f1f4fdae925a3f604c23dcda6eab64bffd04
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-05-25 23:03:37 +0530

    lesson 26 - real time update after new book is created

--- contexts/BooksContext.jsx
+++ contexts/BooksContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import { ID, Permission, Query, Role } from "react-native-appwrite";
-import { databases } from "../lib/appwrite";
+import { databases, client } from "../lib/appwrite";
 import { useUser } from "../hooks/useUser";
 
 const DATABASE_ID = "68334116002bb3b94d6f";
@@ -62,11 +62,40 @@ export function BooksProvider({ children }) {
   }
 
   useEffect(() => {
+    let unsubscribe;
+    const channel = `databases.${DATABASE_ID}.collections.${COLLECTION_ID}.documents`;
+
     if (user) {
       fetchBooks();
+
+      unsubscribe = client.subscribe(channel, (response) => {
+        const { payload, events } = response;
+        console.log(events);
+
+        if (
+          events.includes(
+            `databases.${DATABASE_ID}.collections.${COLLECTION_ID}.documents.create`
+          )
+        ) {
+          setBooks((prevBooks) => {
+            if (prevBooks.some((book) => book.$id === payload.$id))
+              return prevBooks;
+            return [...prevBooks, payload];
+          });
+        }
+        if (events[0].includes("delete")) {
+          setBooks((prevBooks) =>
+            prevBooks.filter((book) => book.$id !== payload.$id)
+          );
+        }
+      });
     } else {
       setBooks([]);
     }
+
+    return () => {
+      if (unsubscribe) unsubscribe();
+    };
   }, [user]);
 
   return (
