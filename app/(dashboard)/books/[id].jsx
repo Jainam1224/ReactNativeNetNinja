50e5ee99fccf9cf0967eab3128f6bff40d660bb3
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-05-25 23:21:08 +0530

    lesson 28 - fetch single book for book details page

--- app/(dashboard)/books/[id].jsx
+++ app/(dashboard)/books/[id].jsx
@@ -9,13 +9,45 @@ import ThemedButton from "../../../components/ThemedButton";
 import ThemedView from "../../../components/ThemedView";
 import Spacer from "../../../components/Spacer";
 import ThemedCard from "../../../components/ThemedCard";
+import { useBooks } from "../../../hooks/useBooks";
+import { useEffect, useState } from "react";
+import ThemedLoader from "../../../components/ThemedLoader";
 
 const BookDetails = () => {
+  const [book, setBook] = useState(null);
+
   const { id } = useLocalSearchParams();
+  const { fetchBookById } = useBooks();
+
+  useEffect(() => {
+    async function loadBook() {
+      const bookData = await fetchBookById(id);
+      setBook(bookData);
+    }
+
+    loadBook();
+  }, [id]);
+
+  if (!book) {
+    return (
+      <ThemedView safe={true} style={styles.container}>
+        <ThemedLoader />
+      </ThemedView>
+    );
+  }
 
   return (
     <ThemedView safe={true} style={styles.container}>
-      <ThemedText>{id}</ThemedText>
+      <ThemedCard style={styles.card}>
+        <ThemedText style={styles.title}>{book.title}</ThemedText>
+        <ThemedText>Written by {book.author}</ThemedText>
+        <Spacer />
+
+        <ThemedText title={true}>Book description:</ThemedText>
+        <Spacer height={10} />
+
+        <ThemedText>{book.description}</ThemedText>
+      </ThemedCard>
     </ThemedView>
   );
 };
@@ -26,5 +58,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "stretch",
+  },
+  title: {
+    fontSize: 22,
+    marginVertical: 10,
+  },
+  card: {
+    margin: 20,
   },
 });
